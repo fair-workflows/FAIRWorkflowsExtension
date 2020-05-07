0aa8d59318bf0ac466f417b19cb25fd7ec929de0
Robin Richardson <raarichardson@gmail.com>
2020-05-07 17:32:04 +0200

    Store search text in React component state, and read from debounced search()

--- src/FAIRSearch.tsx
+++ src/FAIRSearch.tsx
@@ -12,6 +12,93 @@ import { ServerConnection } from '@jupyterlab/services';
 
 import { debounce } from "ts-debounce";
 
+
+export interface IProps {
+    open(openas: string): void;
+}
+
+export interface IState {
+    source: 'nanopub' | 'workflowhub';
+    searchtext: string;
+}
+
+
+class DataExplorer extends React.Component<IProps, IState> {
+    debounced_search: ReturnType<typeof debounce>;
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            source: 'nanopub',
+            searchtext: ''
+        };
+        
+        this.debounced_search = debounce(this.search, 500);
+    }
+
+    onDatasetClick = (event: any) => {
+        console.log(event);
+    }
+
+    onSearchEntry = (event: any) => {
+        this.setState({searchtext: event.target.value});
+        this.debounced_search();
+    }
+
+    search = () => {
+        console.log('searching:', this.state.searchtext);
+    }
+
+    render() {
+        console.log('Rendering DataExplorer')
+        return (
+            <div>
+                <h3>FAIR Search</h3>
+                <input type="text" id="searchentry" name="searchentry" onChange={this.onSearchEntry} value={this.state.searchtext} />
+                <button type="button" onClick={this.onDatasetClick}>Search!</button> 
+            </div>
+        );
+    }
+}
+
+export class TestWidget extends Widget {
+    tracker: INotebookTracker;
+    constructor(tracker: INotebookTracker) {
+        super();
+        this.tracker = tracker;
+        this.title.label = 'FAIRWorkflows';
+        this.title.caption = 'FAIR Workflows';
+        this.id = 'fairworkflowswidget';
+        this.addClass('jp-fairwidget')
+
+        this.update();
+    }
+
+    onUpdateRequest() {
+        console.log('TestWidget onUpdateRequest()');
+
+        ReactDOM.unmountComponentAtNode(this.node);
+        ReactDOM.render(<DataExplorer open={this.onOpen} />, this.node);        
+    }
+
+    onOpen = (openas: string) => {
+        if (!this.tracker.currentWidget) {
+            showErrorMessage('Unable to inject cell without an active notebook', {});
+            return;
+        }
+        const notebook = this.tracker.currentWidget.content;
+        console.log(openas, notebook);
+
+        requestAPI<any>('nanosearch?search_str=fair')
+            .then(data => {
+                console.log(data);
+            })
+            .catch(reason => {
+                console.error('The FAIRWorkflowsExtension server extension appears to be missing.\n${reason}');
+            });
+    }
+
+}
+
 export async function requestAPI<T>(
     endPoint = '',
     init: RequestInit = {}
@@ -43,87 +130,3 @@ export async function requestAPI<T>(
 }
 
 
-
-export interface IProps {
-    open(openas: string): void;
-}
-
-export interface IState {
-    source: 'nanopub' | 'workflowhub';
-}
-
-
-class DataExplorer extends React.Component<IProps, IState> {
-    debounced_search: ReturnType<typeof debounce>;
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            source: 'nanopub'
-        };
-        
-        this.debounced_search = debounce(this.search, 500);
-    }
-
-    onDatasetClick = (event: any) => {
-        console.log(event);
-    }
-
-
-    onSearchEntry = () => {
-        this.debounced_search();
-    }
-
-    search = () => {
-        console.log('searching:');
-    }
-
-    render() {
-        console.log('Rendering DataExplorer')
-        return (
-            <div>
-                <h3>FAIR Search</h3>
-                <input type="text" id="searchentry" name="searchentry" onChange={this.onSearchEntry} />
-                <button type="button" onClick={this.onDatasetClick}>Search!</button> 
-            </div>
-        );
-    }
-}
-
-export class TestWidget extends Widget {
-    tracker: INotebookTracker;
-    constructor(tracker: INotebookTracker) {
-        super();
-        this.tracker = tracker;
-        this.title.label = 'TestWidget';
-        this.title.caption = 'FAIR Workbench';
-        this.id = 'testwidget';
-        this.addClass('jp-fairwidget')
-
-        this.update();
-    }
-
-    onUpdateRequest() {
-        console.log('TestWidget onUpdateRequest()');
-
-        ReactDOM.unmountComponentAtNode(this.node);
-        ReactDOM.render(<DataExplorer open={this.onOpen} />, this.node);        
-    }
-
-    onOpen = (openas: string) => {
-        if (!this.tracker.currentWidget) {
-            showErrorMessage('Unable to inject cell without an active notebook', {});
-            return;
-        }
-        const notebook = this.tracker.currentWidget.content;
-        console.log(openas, notebook);
-
-        requestAPI<any>('nanosearch?search_str=fair')
-            .then(data => {
-                console.log(data);
-            })
-            .catch(reason => {
-                console.error('The FAIRWorkflowsExtension server extension appears to be missing.\n${reason}');
-            });
-    }
-
-}
