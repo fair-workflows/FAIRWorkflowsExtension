ae78e66d620b60db0a18b99ca5cfaf4c8b76b4d2
Robin Richardson <raarichardson@gmail.com>
2020-05-15 17:20:26 +0200

    Separate backend communication (requestAPI) from FAIRSearch source

--- src/FAIRSearch.tsx
+++ src/FAIRSearch.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { URLExt } from '@jupyterlab/coreutils';
-import { ServerConnection } from '@jupyterlab/services';
 import { debounce } from 'ts-debounce';
+import { requestAPI } from './RequestAPI';
 
 /** Properties of the SearchResult component */
 interface ISearchResultProps {
@@ -174,41 +173,3 @@ export class FAIRSearch extends React.Component<IFairSearchProps, IFairSearchSta
         );
     }
 }
-
-
-/**
- * Handles the search query at the specified endpoint (e.g. 'nanopub' or 'workflowhub')
- * and with the specified search parameters (provided through the 'query' dictionary).
- * Returns the results of this request to the extension backend.
- */
-async function requestAPI<T>(
-    endPoint = '',
-    query = {},
-    init: RequestInit = {}
-): Promise<T> {
-    // Make request to Jupyter API
-    const settings = ServerConnection.makeSettings();
-    const queryString = (new URLSearchParams(query)).toString();
-    const requestUrl = URLExt.join(
-        settings.baseUrl,
-        'FAIRWorkflowsExtension', // API Namespace
-        endPoint) + '?' + queryString;
-    
-    console.log('requestAPI called with ' + endPoint + ' ' + init + ', ' + requestUrl);
-
-    let response: Response;
-    try {
-        response = await ServerConnection.makeRequest(requestUrl, init, settings);
-    } catch (error) {
-        throw new ServerConnection.NetworkError(error);
-    }
-
-    const data = await response.json();
-
-    if (!response.ok) {
-        throw new ServerConnection.ResponseError(response, data.message);
-    }
-
-    return data;
-}
-
