95831c86691d1db7c04c86039258d7349007e7ca
Robin Richardson <raarichardson@gmail.com>
2021-04-30 15:09:34 +0200

    Add entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Running entrypoint.sh"
+
+if [[ -v NEWNANOPUBPROFILENAME ]] && [[ -v NEWNANOPUBPROFILEORCID ]]; then
+    setup_nanopub_profile --orcid_id ${NEWNANOPUBPROFILEORCID} --no-publish --name ${NEWNANOPUBPROFILENAME} --newkeys
+fi
+
+if [[ -v NOTEBOOKDIR ]]; then
+    JLDIR="--notebook-dir=${NOTEBOOKDIR}"
+fi
+
+if [[ -v PASSWDHASH ]]; then
+    JLPASS="--NotebookApp.password=${PASSWDHASH}"
+fi
+
+if [[ -v CERTFILE ]]; then
+    JLCERT="--certfile=${CERTFILE}"
+fi
+
+if [[ -v PKEY ]]; then
+    JLKEY="--keyfile=${PKEY}"
+fi
+
+/opt/conda/bin/jupyter lab --ip=0.0.0.0 --port=8888 --allow-root ${JLDIR} ${JLPASS} ${JLCERT} ${JLKEY}
